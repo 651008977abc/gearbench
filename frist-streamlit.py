e6317b6bb8c0fd0a77352b4c55b64f30fda16d57
651008977abc <94041517+651008977abc@users.noreply.github.com>
2021-11-19 16:17:22 +0800

    Add files via upload

---
+++ frist-streamlit.py
@@ -0,0 +1,283 @@
+# Draw a title and some text to the app:
+'''
+# This is the document title
+
+This is some _markdown_.
+'''
+
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+
+    # menu_items={
+    #     'Get Help': 'https://www.extremelycoolapp.com/help',
+    #     'Report a bug': "https://www.extremelycoolapp.com/bug",
+    #     'About': "# This is a header. This is an *extremely* cool app!"
+    # }
+)
+
+df = pd.DataFrame({'col1': [1,2,3]})
+df  # 👈 Draw the dataframe
+
+x = 10
+'x', x  # 👈 Draw the string 'x' and then the value of x
+
+# Also works with most supported chart types
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+fig  # 👈 Draw a Matplotlib chart
+
+st.markdown('Streamlit is **_really_ cool**.')
+st.title('This is a title')
+st.header('This is a header')
+st.subheader('This is a subheader')
+st.caption('This is a string that explains something above.')
+st.text('This is some text.')
+
+code = '''def hello():
+...     print("Hello, Streamlit!")'''
+st.code(code, language='python')
+st.latex(r'''
+...     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+...     \sum_{k=0}^{n-1} ar^k =
+...     a \left(\frac{1-r^{n}}{1-r}\right)
+...     ''')
+
+df = pd.DataFrame(np.random.randn(50, 20),columns=('col %d' % i for i in range(20)))
+st.dataframe(df)  # Same as st.write(df)
+
+df2 = pd.DataFrame(
+   np.random.randn(10, 5),
+   columns=('col %d' % i for i in range(5)))
+
+st.table(df2)
+
+st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
+
+st.metric(label="Gas price", value=4, delta=-0.5,
+    delta_color="inverse")
+
+st.metric(label="Active developers", value=123, delta=123,
+    delta_color="off")
+
+st.json({
+    'foo': 'bar',
+    'baz': 'boz',
+    'stuff': [
+        'stuff 1',
+        'stuff 2',
+        'stuff 3',
+        'stuff 5',
+    ],
+ })
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+st.line_chart(chart_data)
+st.bar_chart(chart_data)
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
+
+
+import streamlit as st
+import plotly.figure_factory as ff
+import numpy as np
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+ # Plot!
+st.plotly_chart(fig, use_container_width=True)
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
+
+agree = st.checkbox('I agree')
+
+if agree:
+    st.write('Great!')
+
+genre = st.radio(
+   "What's your favorite movie genre",
+    ('Comedy', 'Drama', 'Documentary'))
+
+if genre == 'Comedy':
+    st.write('You selected comedy.')
+else:
+    st.write("You didn't select comedy.")
+option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected:', option)
+options = st.multiselect(
+    'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
+values = st.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+#C:\Users\CNXIMEN1\PycharmProjects\streamlit\frist-streamlit.py
+start_color, end_color = st.select_slider(
+    'Select a range of color wavelength',
+    options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
+    value=('red', 'blue'))
+st.write('You selected wavelengths between', start_color, 'and', end_color)
+
+title = st.text_input('Movie title', 'Life of Brian')
+st.write('The current movie title is', title)
+
+number = st.number_input('Insert a number')
+st.write('The current number is ', number)
+
+txt = st.text_area('Text to analyze', '''
+...     It was the best of times, it was the worst of times, it was
+...     the age of wisdom, it was the age of foolishness, it was
+...     the epoch of belief, it was the epoch of incredulity, it
+...     was the season of Light, it was the season of Darkness, it
+...     was the spring of hope, it was the winter of despair, (...)
+...     ''')
+st.write('Sentiment:', txt)
+import datetime
+d = st.date_input(
+    "When's your birthday",
+    datetime.date(2019, 7, 6))
+st.write('Your birthday is:', d)
+
+t = st.time_input('Set an alarm for', datetime.time(8, 45))
+st.write('Alarm is set for', t)
+color = st.color_picker('Pick A Color', '#00f900')
+st.write('The current color is', color)
+from PIL import Image
+image = Image.open('123.jpg')
+
+st.image(image, caption='Sunrise by the mountains')
+
+import streamlit as st
+
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email2", "Home phone2", "Mobile phone2")
+)
+
+col1, col2 = st.columns([3, 1])
+data = np.random.randn(10, 1)
+
+col1.subheader("A wide column with a chart")
+col1.line_chart(data)
+
+col2.subheader("A narrow column with the data")
+col2.write(data)
+
+
+st.line_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+with st.expander("See explanation"):
+    st.write("""
+...         The chart above shows some numbers I picked for you.
+...         I rolled actual dice for these, so they're *guaranteed* to
+...         be random.
+...     """)
+    st.image("https://static.streamlit.io/examples/dice.jpg")
+with st.container():
+   st.write("This is inside the container")
+
+   # You can call any Streamlit command, including custom components:
+   st.bar_chart(np.random.randn(50, 3))
+st.write("This is outside the container")
+
+import time
+
+# with st.empty():
+#     for seconds in range(60):
+#         st.write(f"⏳ {seconds} seconds have passed")
+#         time.sleep(1)
+#     st.write("✔️ 1 minute over!")
+
+# >>> import time
+
+# my_bar = st.progress(0)
+#
+# for percent_complete in range(100):
+#     time.sleep(0.1)
+#     my_bar.progress(percent_complete + 1)
+#
+# with st.spinner('Wait for it...'):
+#     time.sleep(5)
+st.success('Done!')
+# st.balloons()
+st.error('This is an error')
+st.warning('This is a warning')
+st.info('This is a purely informational message')
+st.success('This is a success message!')
+e = RuntimeError('This is an exception of type RuntimeError')
+st.exception(e)
+
+# name = st.text_input('Name')
+# if not name:
+#     st.warning('Please input a name.')
+#     st.stop()
+# st.success('Thank you for inputting a name.')
+
+with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
+
+df1 = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20)))
+
+my_table = st.table(df1)
+
+df2 = pd.DataFrame(
+    np.random.randn(50, 20),
+    columns=('col %d' % i for i in range(20)))
+
+my_table.add_rows(df2)
+# Now the table shown in the Streamlit app contains the data for
+# df1 followed by the data for df2.
